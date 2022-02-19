493bcb8f72d48c135595480d60750f56e1139190
Junhwan26 <gjwnsghks02@gmail.com>
2022-02-19 17:23:09 +0900

    22/02/19

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import requests
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+url = "https://api.upbit.com/v1/market/all?isDetails=false"
 
+headers = {"Accept": "application/json"}
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+response = requests.request("GET", url, headers=headers)
 
+print(response.text)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
